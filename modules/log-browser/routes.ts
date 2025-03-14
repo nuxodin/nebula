7fc6470927aa9cb53c2d3cce70836bfcd543d063
Tobias Buschor <tobias.buschor@shwups.ch>
2025-03-14 15:02:10 +0100

    asdf

--- modules/log-browser/routes.ts
+++ modules/log-browser/routes.ts
@@ -10,8 +10,8 @@ objToRoutes(apiRoutes, api);
 // View Routes
 const viewRoutes = new Hono();
 viewRoutes.get("/", async (c: Context) => {
-    const content = await Deno.readTextFile("./modules/log-browser2/views/content.html");
-    const scripts = await Deno.readTextFile("./modules/log-browser2/views/scripts.html");
+    const content = await Deno.readTextFile("./modules/log-browser/views/content.html");
+    const scripts = await Deno.readTextFile("./modules/log-browser/views/scripts.html");
     return c.html(await renderTemplate("Log Browser 2.0", content, "", scripts));
 });
 
